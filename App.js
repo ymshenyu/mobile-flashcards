853446e4f8defb82645224a2e4f9a4d2934267c1
ymshenyu <ymshenyu@gmail.com>
2020-06-15 14:53:29 +0800

    changes for tab navigator

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons/'
+import Decks from './components/Decks'
+import AddDeck from './components/AddDeck'
+
+const Tab = createMaterialBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator barStyle={styles.tab} initialRouteName='Decks' activeColor='#4287f5'>
+        <Tab.Screen name='Decks' component={Decks} options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name='book-open' size={24} color={color} />
+          )
+        }} />
+        <Tab.Screen name='Add Deck' component={AddDeck} options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name='add-box' size={24} color={color} />
+          )
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +35,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  tab: {
+    backgroundColor: 'white',
+  },
+})
---
+++ components/Decks.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default () => {
+    return (
+        <View style={styles.container}>
+            <Text>
+                Decks
+            </Text>
+        </View>
+    )
+}
+
